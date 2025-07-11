cd511ec6f15b0c096c47d199d127755415e0e654
Boat2356 <chonlaphat.p@kkumail.com>
2025-07-11 17:36:49 +0700

    Change model typhoon-ocr-7b to typhoon-ocr-3b

--- Typhoon_OCR_Demo.py
+++ Typhoon_OCR_Demo.py
@@ -1,41 +1,48 @@
-import base64
-from openai import OpenAI
+# import base64
+# from openai import OpenAI
 
-client = OpenAI(base_url="http://localhost:1234/v1", api_key="not-needed")
+# client = OpenAI(base_url="http://localhost:1234/v1", api_key="not-needed")
 
-def encode_image_to_base64(image_path):
-  with open(image_path, "rb") as f:
-      return base64.b64encode(f.read()).decode("utf-8")
+# def encode_image_to_base64(image_path):
+#   with open(image_path, "rb") as f:
+#       return base64.b64encode(f.read()).decode("utf-8")
 
-image_path = "./Invoice/Thapthai.png"
-image_data = encode_image_to_base64(image_path)
+# image_path = "./Invoice/Thapthai.png"
+# image_data = encode_image_to_base64(image_path)
 
-response = client.chat.completions.create(
-  model="typhoon-ocr-3b",
-  messages=[
-      {
-          "role": "system",
-          "content": "You are an OCR model. Extract all readable text from the image."
-      },
-      {
-          "role": "user",
-          "content": [
-              {"type": "text", "text": "Please extract text from this image:"},
-              {
-                  "type": "image_url",
-                  "image_url": {
-                      "url": f"data:image/png;base64,{image_data}"
-                  }
-              }
-          ]
-      }
-  ],
-  max_tokens=16384,
-  extra_body={
-              "repetition_penalty": 1.2,
-              "temperature": 0.1,
-              "top_p": 0.6,
-          },
-)
+# response = client.chat.completions.create(
+#   model="typhoon-ocr-3b",
+#   messages=[
+#       {
+#           "role": "system",
+#           "content": "You are an OCR model. Extract all readable text from the image."
+#       },
+#       {
+#           "role": "user",
+#           "content": [
+#               {"type": "text", "text": "Please extract text from this image:"},
+#               {
+#                   "type": "image_url",
+#                   "image_url": {
+#                       "url": f"data:image/png;base64,{image_data}"
+#                   }
+#               }
+#           ]
+#       }
+#   ],
+#   max_tokens=16384,
+#   extra_body={
+#               "repetition_penalty": 1.2,
+#               "temperature": 0.1,
+#               "top_p": 0.6,
+#           },
+# )
 
-print(response.choices[0].message.content)
+# print(response.choices[0].message.content)
+
+
+# Local Model Via VLLM
+from typhoon_ocr import ocr_document
+markdown = ocr_document('./Invoice/Thapthai.png', base_url='http://localhost:8000/v1', api_key='no-key')
+print(markdown)
+
